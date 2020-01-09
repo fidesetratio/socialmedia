1fa0a605fade95c10dfd962661ce94f08d8b6546
Patar.Tambunan <Patar.Tambunan@itpega01.SINARMASMSIGLIFE.CO.ID>
2020-01-09 10:54:54 +0700

    commit and push

---
+++ src/main/java/com/app/conf/AppWebConfig.java
@@ -0,0 +1,26 @@
+package com.app.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//https://github.com/endymuhardin/training-brainmatics-2015-1/tree/master/aplikasi-pelatihan/src/main/java/com/muhardin/endy/pelatihan/config
+@Configuration
+public class AppWebConfig implements  WebMvcConfigurer  {
+	
+		@Override
+	    public void addViewControllers(ViewControllerRegistry registry) {
+	        registry.addViewController("/hello").setViewName("hello");
+	    };
+	    
+	    
+/*
+	    @Override
+	    public void addInterceptors(InterceptorRegistry registry) {
+	        registry.addInterceptor(new ThymeleafLayoutInterceptor());
+	    }*/
+	    
+	
+	
+}
