7bf7b2153004d39bc46691deb346f0b09ac850a0
root <root@patartimotius01>
2020-01-14 21:28:34 +0700

    change interceptor

--- src/main/java/com/app/conf/ThymeleafLayoutInterceptor.java
+++ src/main/java/com/app/conf/ThymeleafLayoutInterceptor.java
@@ -13,14 +13,39 @@ public class ThymeleafLayoutInterceptor extends HandlerInterceptorAdapter {
 	 
 	    @Override
 	    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-	        if (modelAndView == null || !modelAndView.hasView()) {
+	        
+	    	Boolean allowed = (Boolean)request.getAttribute("allowed");
+	    	if (modelAndView == null || !modelAndView.hasView()) {
 	            return;
 	        }
-	        String originalViewName = modelAndView.getViewName();
-	        if(originalViewName.startsWith("redirect:") || originalViewName.startsWith("report")){
-	            return;
-	        }
-	        modelAndView.setViewName(DEFAULT_LAYOUT);
-	        modelAndView.addObject(DEFAULT_VIEW_ATTRIBUTE_NAME, originalViewName);
-	    }    
+
+	    	String originalViewName = modelAndView.getViewName();
+	    	if(allowed) {
+	    		if(originalViewName.startsWith("redirect:") || originalViewName.startsWith("report")){
+		            return;
+		        }
+		        modelAndView.setViewName(DEFAULT_LAYOUT);
+		        modelAndView.addObject(DEFAULT_VIEW_ATTRIBUTE_NAME, originalViewName);
+		    	
+	    	}else {
+	    		modelAndView.setViewName(originalViewName);
+	    	}
+	    	
+	    }
+
+		@Override
+		public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+				throws Exception {
+			// TODO Auto-generated method stub
+			System.out.println("request URI:"+request.getRequestURI());
+			request.setAttribute("allowed",new Boolean(true));
+			
+			if(request.getRequestURI().contains("login")) {
+				request.setAttribute("allowed",new Boolean(false));
+						
+			}
+			return super.preHandle(request, response, handler);
+		}    
+	    
+	    
 }
